f9f0391e4cef1cc2a3cfb27f4f3b6d67501810a8
SarifulSarker <sarifulsarker19@gmail.com>
2025-04-26 01:09:52 +0600

    Update package and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // ✅ This is required for GitHub Pages
   plugins: [
-    react(), 
-    tailwindcss(),],
+    react(),
+    tailwindcss(),
+  ],
 })
